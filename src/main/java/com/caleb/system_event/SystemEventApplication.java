285ec478b0336ed4d2985193d9aa2f69fa230412
Marcelo Caleb Júlio Ofice <marcelocalebjulioo@gmail.com>
2025-10-20 16:36:39 +0200

    Project created

---
+++ src/main/java/com/caleb/system_event/SystemEventApplication.java
@@ -0,0 +1,13 @@
+package com.caleb.system_event;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemEventApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemEventApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/caleb/system_event/SystemEventApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caleb.system_event;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemEventApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
